eb3ef2a58c2adb6c135a67b439220a0653d03e5f
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-18 07:29:37 -0700

    Creating MoneyAfricaGuestPageFooter

--- src/components/MoneyAfricaGuestPage/MoneyAfricaGuestPage.js
+++ src/components/MoneyAfricaGuestPage/MoneyAfricaGuestPage.js
@@ -13,11 +13,8 @@ import MoneyAfricaVideoDescription from "../../images/MoneyAfricaVideoDescriptio
 import MoneyAfricaBookImage from "../../images/MoneyAfricaBookImage.png";
 import PlayBook from "../../images/PlayBook.png";
 import WalletImage from "../../images/WalletImage.png";
-import FooterLogoImage from "../../images/FooterLogoImage.png";
-import Facebook from "../../images/Facebook.png";
-import WhatsappImage from "../../images/WhatsappImage.png";
-import Linkdin from "../../images/Linkdin.png";
-import Twitter from "../../images/Twitter.png";
+import FooterComponent from "../FooterComponent/FooterComponent";
+
 const MoneyAfricaGuestPage = () => {
   return (
     <React.Fragment>
@@ -154,7 +151,6 @@ const MoneyAfricaGuestPage = () => {
             </div>
           </div>
         </div>
-
         <div className="container">
           <div className="df justify-content-betw">
             <div>
@@ -197,53 +193,6 @@ const MoneyAfricaGuestPage = () => {
             <div>
               <img src={WalletImage} />
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="FooterStyles padding-1">
-        <div className="df justify-content-betw">
-          <div>
-            <img src={FooterLogoImage} />
-            <p className="footerText text-font">
-              © 2021 The Money Africa. All rights reserved.
-            </p>
-            <div className="df">
-              <div className="PaddingForIcon">
-                <img src={Facebook} />
-              </div>
-              <div className="PaddingForIcon">
-                <img src={Linkdin} />
-              </div>
-              <div className="PaddingForIcon">
-                <img src={Twitter} />
-              </div>
-              <div className="PaddingForIcon">
-                <img src={WhatsappImage} />
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="text-font font-bolder">SERVICES</p>
-            <p className="text-font SpaceAllFooterText">MA Premium</p>
-            <p className="text-font SpaceAllFooterText">
-              MA Learning platform{" "}
-            </p>
-            <p className="text-font SpaceAllFooterText">MA Kids</p>
-            <p className="text-font SpaceAllFooterText">MA Communities</p>
-            <p className="text-font SpaceAllFooterText">Digital Advisor</p>
-          </div>
-          <div>
-            <p className="text-font font-bolder">COMPANY</p>
-            <p className="text-font SpaceAllFooterText">About Us</p>
-            <p className="text-font SpaceAllFooterText">Careers</p>
-            <p className="text-font SpaceAllFooterText">Contact Us</p>
-          </div>
-          <div>
-            <p className="text-font font-bolder">TOOLS</p>
-            <p className="text-font SpaceAllFooterText">Calculator</p>
-          </div>
-          <div>
-            <p className="text-font font-bolder">Sign up for the newsletter</p>
           </div>
         </div>
       </div>
