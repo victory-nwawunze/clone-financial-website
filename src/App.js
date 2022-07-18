ec2ca928d652adc3207fe859f73c755474847506
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-18 09:07:44 -0700

    Creating MoneyAfricaContactUsPage

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import FooterComponent from "./components/FooterComponent/FooterComponent";
 import MoneyAfricaContactUsPage from "./components/MoneyAfricaContactUsPage/MoneyAfricaContactUsPage";
 import MoneyAfricaGuestPage from "./components/MoneyAfricaGuestPage/MoneyAfricaGuestPage";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
       {/* <Navbar /> */}
-      <MoneyAfricaGuestPage />
-      <MoneyAfricaContactUsPage />
+      <Routes>
+        <Route path={"/"} element={<Navigate to={"moneyAfrica/Guest"} />} />
+        <Route path={"/moneyAfrica/Guest"} element={<MoneyAfricaGuestPage />} />
+        <Route
+          path={"/moneyAfrica/ContactUs"}
+          element={<MoneyAfricaContactUsPage />}
+        />
+      </Routes>
+
       <FooterComponent />
     </div>
   );
--- src/components/MoneyAfricaContactUsPage/MoneyAfricaContactUsPage.js
+++ src/components/MoneyAfricaContactUsPage/MoneyAfricaContactUsPage.js
@@ -5,7 +5,17 @@ import React from "react";
 const MoneyAfricaContactUsPage = () => {
   return (
     <React.Fragment>
-      <h1>MoneyAfricaContactUsPage</h1>
+      <div className="MoneyAfricaContactUsPage padding-1">
+        <p className="text-center ContactUsText">
+          We’re here to help you figure out
+          <br /> all your questions about money.
+        </p>
+        <p className="text-center ContactUsTextFont text-font">
+          Would you like a collaboration, expert advice or more helpful
+          information
+          <br /> about our services?. Please kindly drop us a message.
+        </p>
+      </div>
     </React.Fragment>
   );
 };
