93b9ef7abfd1bb3c0ede4b39038bacf24150005f
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-18 03:51:35 -0700

    Creating MoneyAfricaGuestPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /** @format */
 
+import MoneyAfricaGuestPage from "./components/MoneyAfricaGuestPage/MoneyAfricaGuestPage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      <MoneyAfricaGuestPage />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,75 +1,79 @@
 /** @format */
 
-import React from "react";
-import MoneyAfricaLogo from "../../images/MoneyAfricaLogo.png";
-import ChevDown from "../../images/ChevDown.png";
-const Navbar = () => {
-  return (
-    <React.Fragment>
-      <div className="StyleNavbarComponent">
-        <div className="container ptb-2">
-          <div className="df jcsb">
-            <div>
-              <img src={MoneyAfricaLogo} />
-            </div>
-            <div>
-              <div className="df">
-                <div>
-                  <div className="df">
-                    <div>
-                      <p>Service</p>
-                    </div>
-                    <div>
-                      <img src={ChevDown} />
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <div className="df">
-                    <div>
-                      <p>Tools</p>
-                    </div>
-                    <div>
-                      <p>2</p>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <div className="df">
-                    <div>Company</div>
-                    <div>2</div>
-                  </div>
-                </div>
-                <div>
-                  <div className="df">
-                    <div>
-                      <p>Explore</p>
-                    </div>
-                    <div>2</div>
-                  </div>
-                </div>
-                <div>
-                  <div className="df">
-                    <div>
-                      <p>Login</p>
-                    </div>
-                    <div>2</div>
-                  </div>
-                </div>
-                <div>
-                  <div className="df">
-                    <div>1</div>
-                    <div>2</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>3</div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
-};
+// /** @format */
 
-export default Navbar;
+// import React from "react";
+// import MoneyAfricaLogo from "../../images/MoneyAfricaLogo.png";
+// import ChevDown from "../../images/ChevDown.png";
+// import ArrowRight from "../../images/Arrow.png";
+// const Navbar = () => {
+//   return (
+//     <React.Fragment>
+//       <div className="NavbarStylesComponent ">
+//         <div className=" padding-1">
+//           <div className="df jcsb">
+//             <div className="PaddingRight PaddingTop">
+//               <img src={MoneyAfricaLogo} />
+//             </div>
+//             <div className="PaddingTop">
+//               <div className="df">
+//                 <div>
+//                   <div className="df">
+//                     <div className="PaddingRightForNavNext">
+//                       <p className="text-font">
+//                         Service{" "}
+//                         <span className="ChevDownPaddingBottom">
+//                           <img src={ChevDown} />
+//                         </span>
+//                       </p>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div>
+//                   <div className="df">
+//                     <div className="PaddingRightForNavNext">
+//                       <p className="text-font ">Tools</p>
+//                     </div>
+//                     <div>
+//                       <img src={ChevDown} />
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div>
+//                   <div className="df">
+//                     <div className="PaddingRightForNavNext">
+//                       <p className="text-font">Company</p>
+//                     </div>
+//                     <div>
+//                       <img src={ChevDown} />
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div>
+//                   <div className="df">
+//                     <div className="PaddingRightForNavNext">
+//                       <p className="text-font">Explore</p>
+//                     </div>
+//                     <div>
+//                       <img src={ChevDown} />
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="PaddingRightForNavNext">
+//                   <p className="text-font">Login</p>
+//                 </div>
+//                 <div className="text-font PaddingRightForNavNext">
+//                   Get Started
+//                 </div>
+//                 <div>7</div>
+//               </div>
+//             </div>
+//             <div className="PaddingLeft PaddingTop">1</div>
+//           </div>
+//         </div>
+//       </div>
+//     </React.Fragment>
+//   );
+// };
+
+// export default Navbar;
