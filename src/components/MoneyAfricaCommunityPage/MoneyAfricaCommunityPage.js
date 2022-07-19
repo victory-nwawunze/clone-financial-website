6e85e755cb5dd72d8f4309364247de779579d31d
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-19 08:57:57 -0700

    Finished MoneyAfricaCommunityPage

--- src/components/MoneyAfricaCommunityPage/MoneyAfricaCommunityPage.js
+++ src/components/MoneyAfricaCommunityPage/MoneyAfricaCommunityPage.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import React from "react";
-
+import MoneyAfricaCommunityImage from "../../images/MoneyAfricaCommunityImage.png";
+import MoneyAfricaCommunityMarkedIcon from "../../images/CommunityMarkedIcon.png";
 const MoneyAfricaCommunityPage = () => {
   return (
     <React.Fragment>
@@ -15,6 +16,90 @@ const MoneyAfricaCommunityPage = () => {
           value="Join our community"
           className="JoinCommuntity"
         />
+      </div>
+      <div className="MoneyAfricaCommunityMembers">
+        <div className="df justify-content-betw">
+          <div className="MarginToWidth">
+            <img src={MoneyAfricaCommunityImage} className="ImageSizing" />
+          </div>
+          <div className="MarginToCommunityText">
+            <p className="TextStylesForCommunity">
+              Benefits of becoming a<br /> commmunity member
+            </p>
+            <div className="MoneyAfricaTextWithMarkIcon">
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Real-time engagements and networking
+                  </p>
+                </div>
+              </div>
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Access to resources to support your goals
+                  </p>
+                </div>
+              </div>
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Take online courses on personal finance
+                  </p>
+                </div>
+              </div>
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Ask questions and get support from the community
+                  </p>
+                </div>
+              </div>
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Have access to global and local market intelligence
+                  </p>
+                </div>
+              </div>
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Get access to private investment deals
+                  </p>
+                </div>
+              </div>
+              <div className="df">
+                <div>
+                  <img src={MoneyAfricaCommunityMarkedIcon} />
+                </div>
+                <div>
+                  <p className="SpaceAllCommunityText">
+                    Access to weekly webinars and newsletters
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
