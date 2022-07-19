9755530143a1151c00b143d43c07c93ce65330ff
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-19 05:53:42 -0700

    Finished MoneyAfricaPremiumPageAccordion

--- src/components/MoneyAfricaPremiumPage/MoneyAfricaPremiumPage.js
+++ src/components/MoneyAfricaPremiumPage/MoneyAfricaPremiumPage.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import MarkIcon from "../../images/MarkIcon.png";
+import AccordionPlusImage from "../../images/AccordionPlusImage.png";
 const MoneyAfricaPremiumPage = () => {
   return (
     <React.Fragment>
@@ -176,6 +177,39 @@ const MoneyAfricaPremiumPage = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="MoneyAfricaAAccordion padding-1">
+        <p className="text-center FAQStylesText">Frequently asked questions</p>
+        <p className="FAQtextStyles">
+          Can i have a personal instructor or do i have to just watch the course
+          videos?
+        </p>
+        <div className="PositionAccordionStylesWithLines">
+          <div className="PositionPlusImage">
+            <img src={AccordionPlusImage} />
+          </div>
+        </div>
+        <hr className="styleAccordionLine" />
+        <p className="FAQtextStyles">
+          How do i enroll on the MoneyAfrica platform?
+        </p>
+        <div className="PositionAccordionStylesWithLines">
+          <div className="PositionPlusImage">
+            <img src={AccordionPlusImage} />
+          </div>
+        </div>
+        <hr className="styleAccordionLine" />
+        <p className="FAQtextStyles">What is the duration for the course?</p>
+        <div className="PositionAccordionStylesWithLines">
+          <div className="PositionPlusImage">
+            <img src={AccordionPlusImage} />
+          </div>
+        </div>
+        <hr className="styleAccordionLine" />
+        <p className="text-center SpacingForAccordionLink">
+          Have more enquiries?
+          <span className="SendUsAMessageTextStyles"> Send us a message</span>
+        </p>
       </div>
     </React.Fragment>
   );
