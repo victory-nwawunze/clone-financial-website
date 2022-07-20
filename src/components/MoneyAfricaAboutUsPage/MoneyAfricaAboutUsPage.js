54215fee9c25a32f96e0501dc4a3351cde510de8
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-20 03:27:02 -0700

    Creating MoneyAfricaAbtPageTexts

--- src/components/MoneyAfricaAboutUsPage/MoneyAfricaAboutUsPage.js
+++ src/components/MoneyAfricaAboutUsPage/MoneyAfricaAboutUsPage.js
@@ -20,6 +20,37 @@ const MoneyAfricaAboutUsPage = () => {
           <img src={PlayIcon} className="StylePlayIcon" />
         </div>
       </div>
+      <div className="MoneyAfricaAboutUsDescriptionStyles">
+        <div className="df justify-content-betw">
+          <div>
+            <p className="SimplifyingTextStyles">
+              Simplifying financial
+              <br /> education since 2012
+            </p>
+          </div>
+          <div>
+            <p className="MoneyAfricaAbtDesTextOne">
+              MoneyAfrica was launched following the founders' frustration with
+              existing
+              <br /> CRM services that were either overly simplistic or far too
+              complex for most
+              <br /> businesses.
+            </p>
+            <p className="MoneyAfricaAbtDesTextTwo">
+              We built MoneyAfrica to deliver on these values and today
+              MoneyAfrica is
+              <br /> used by thousands of businesses of all sizes all over the
+              world.
+            </p>
+            <p className="MoneyAfricaAbtDesTextThree">
+              We're based in Lagos, Nigeria, a city with a creative heart that
+              was founded
+              <br /> on science and industry and the birthplace of the modern
+              computer.
+            </p>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
