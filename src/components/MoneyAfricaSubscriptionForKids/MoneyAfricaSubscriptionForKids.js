2322cbb9cb904ee48c3b9a102e586b562905d369
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-21 08:47:01 -0700

    Created MoneyAfricaSubscriptionForKidsCards

--- src/components/MoneyAfricaSubscriptionForKids/MoneyAfricaSubscriptionForKids.js
+++ src/components/MoneyAfricaSubscriptionForKids/MoneyAfricaSubscriptionForKids.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-
+import SummerBootCampArrow from "../../images/SummerBootCampArrow.png";
 const MoneyAfricaSubscriptionForKids = () => {
   return (
     <React.Fragment>
@@ -17,6 +17,44 @@ const MoneyAfricaSubscriptionForKids = () => {
           Our boot camps help kids acquire financial literacy
           <br /> skills in a fun way.
         </p>
+      </div>
+      <div className="MoneyAfricaForKidsCard">
+        <div className="df justify-content-betw">
+          <div>
+            <div className="MoneyAfricaForKidsCardOne">
+              <p className="SummerBootcampTextStyles">Summer Boot Camp</p>
+              <p className="SummerBootcampDateStyles">
+                (August – September 2021)
+              </p>
+              <p className="SummerBootcampDiscountTextStyles">
+                *up to 20% discount for siblings*
+              </p>
+              <button className="BTNSummerBootCampButtonStyles">
+                Enrol your kids
+                <span>
+                  <img src={SummerBootCampArrow} />
+                </span>
+              </button>
+            </div>
+          </div>
+          <div>
+            <div className="MoneyAfricaForKidsCardTwo">
+              <p className="SummerBootcampTextStyles">Summer Boot Camp</p>
+              <p className="SummerBootcampDateStyles">
+                (August – September 2021)
+              </p>
+              <p className="SummerBootcampDiscountTextStyles">
+                *up to 20% discount for siblings*
+              </p>
+              <button className="BTNSummerBootCampButtonStyles">
+                Enrol your kids
+                <span>
+                  <img src={SummerBootCampArrow} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
