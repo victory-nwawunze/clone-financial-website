20d2c2880d6322afa1007db7d2517e0efc7e1e71
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-22 03:45:15 -0700

    Creating MoneyAfricaDigitalAdvisoryPageCards

--- src/components/MoneyAfricaDigitalAdvisoryPage/MoneyAfricaDigitalAdvisoryPage.js
+++ src/components/MoneyAfricaDigitalAdvisoryPage/MoneyAfricaDigitalAdvisoryPage.js
@@ -11,6 +11,35 @@ const MoneyAfricaDigitalAdvisoryPage = () => {
           <br /> investment advisory
         </p>
       </div>
+      <div className="MoneyAfricaDigitalAdvisoryCards">
+        <div className="df justify-content-betw">
+          <div>
+            <p className="GYFTextStyles">Grow your financial muscle</p>
+            <p className="MoreGYFTextStyles">
+              Improve your financial health and investment
+              <br /> strategy by answering a few questions
+            </p>
+          </div>
+          <div className="MoneyAfricaDigitalAdvisoryCardBorder">
+            <p className="FHCstyles">Financial Health Check</p>
+            <p className="FreeAndYearlyStylesText">
+              Free <span className="PerYearStylesText">/per year</span>
+            </p>
+            <hr className="LineFreeAndYearlyStylesText" />
+            <p className="STSTextStyle">
+              <span className="MoreSTSTextStyle"> Short-term stability –</span>{" "}
+              emergency funds,
+              <br /> health insurance, short-term savings strategy.
+            </p>
+            <p className="STSTextStyleTwo">
+              <span className="MoreSTSTextStyle">Long-term security –</span>{" "}
+              retirement plan,
+              <br /> investment strategy.
+            </p>
+          </div>
+          <div>3</div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
