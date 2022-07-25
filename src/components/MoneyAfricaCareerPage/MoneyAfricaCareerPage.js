dacde2a4dff37a7004bb6eeec87ec34e612b5ee2
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-25 06:20:28 -0700

    Creating MoneyAfricaCareerPageForJobAvailabilty

--- src/components/MoneyAfricaCareerPage/MoneyAfricaCareerPage.js
+++ src/components/MoneyAfricaCareerPage/MoneyAfricaCareerPage.js
@@ -4,39 +4,13 @@ import React from "react";
 import RightArrImage from "../../images/ArrowRightImage.png";
 import CareerImage from "../../images/CareerImage.png";
 import LineWithCirleImage from "../../images/LineWithCirleImage.png";
+import MoneyAfricaBannerTextWithBackgroundColor from "../MoneyAfricaBannerTextWithBackgroundColor/MoneyAfricaBannerTextWithBackgroundColor";
 import MoneyAfricaCareerPageBannerComponent from "../MoneyAfricaCareerPageBannerComponent/MoneyAfricaCareerPageBannerComponent";
 const MoneyAfricaCareerPage = () => {
   return (
     <React.Fragment>
       <MoneyAfricaCareerPageBannerComponent />
-      <div className="ValueThatDefinesUsTextStyles">
-        <h3 className="VADUStyles">Values that define us</h3>
-        <div className="FlexAllValueTextStyles">
-          <div className="df">
-            <div>
-              <h4 className="DontSayStyles">Don't say nothing</h4>
-              <p className="SYMStylesText">
-                Speak your mind. Challenge. Cheer.
-                <br /> Converse. Do it honestly and respectfully.
-              </p>
-            </div>
-            <div>
-              <h4 className="DontSayStyles">Own the outcome</h4>
-              <p className="SYMStylesText">
-                Be invested beyond role, experience or
-                <br /> title. You are the master of one.
-              </p>
-            </div>
-            <div>
-              <h4 className="DontSayStyles">Change shoes often</h4>
-              <p className="SYMStylesText">
-                With ourselves. With our users. And
-                <br /> without judgment.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MoneyAfricaBannerTextWithBackgroundColor />
       <div className="AvailablePositionStyles">
         <h4 className="APStylesText">Available positions</h4>
       </div>
--- src/components/MoneyAfricaCareerPageForJobAvailabilty/MoneyAfricaCareerPageForJobAvailabilty.js
+++ src/components/MoneyAfricaCareerPageForJobAvailabilty/MoneyAfricaCareerPageForJobAvailabilty.js
@@ -1,12 +1,22 @@
 /** @format */
 
 import React from "react";
+import MoneyAfricaBannerTextWithBackgroundColor from "../MoneyAfricaBannerTextWithBackgroundColor/MoneyAfricaBannerTextWithBackgroundColor";
 import MoneyAfricaCareerPageBannerComponent from "../MoneyAfricaCareerPageBannerComponent/MoneyAfricaCareerPageBannerComponent";
+import BriefCaseImage from "../../images/BriefCaseImage.png";
 
 const MoneyAfricaCareerPageForJobAvailabilty = () => {
   return (
     <React.Fragment>
       <MoneyAfricaCareerPageBannerComponent />
+      <MoneyAfricaBannerTextWithBackgroundColor />
+      <div className="StylesAvailabeTextFonts">
+        <h4>Available positions</h4>
+      </div>
+      <div className="JobStatusWithImage">
+        <img src={BriefCaseImage} />
+        <p>No available jobs now</p>
+      </div>
     </React.Fragment>
   );
 };
