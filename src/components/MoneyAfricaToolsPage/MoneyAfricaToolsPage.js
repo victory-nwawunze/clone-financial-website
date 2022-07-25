07c0d63c24e863405b09d76d154e571eae15f46b
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-25 07:40:44 -0700

    Creating MoneyAfricaToolsPage

---
+++ src/components/MoneyAfricaToolsPage/MoneyAfricaToolsPage.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from "react";
+
+const MoneyAfricaToolsPage = () => {
+  return (
+    <React.Fragment>
+      <div className="MoneyAfricaToolsPageBannerStyles">
+        <h3 className="InvestMentTextStyles">Investment calculators & tools</h3>
+        <p className="MoreInvestMentTextStyles">
+          Strengthen your financial strategy with this toolbox of do-it-yourself
+          <br />
+          resources, or get personal financial advice from our advisors.{" "}
+        </p>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MoneyAfricaToolsPage;
