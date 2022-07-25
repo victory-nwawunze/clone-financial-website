30161e42f1fba2e3adf5ba3a5e1cb94c516f1779
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-25 09:17:21 -0700

    Creating MoneyAfricaToolsPage

--- src/components/MoneyAfricaToolsPage/MoneyAfricaToolsPage.js
+++ src/components/MoneyAfricaToolsPage/MoneyAfricaToolsPage.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-
+import RightArrImage from "../../images/RightArrImage.png";
 const MoneyAfricaToolsPage = () => {
   return (
     <React.Fragment>
@@ -12,6 +12,56 @@ const MoneyAfricaToolsPage = () => {
           <br />
           resources, or get personal financial advice from our advisors.{" "}
         </p>
+      </div>
+      <div className="MoneyAfricaToolPageCardStyles">
+        <div className="df justify-content-betw">
+          <div>
+            <div className="MoneyAfricaToolsCard">
+              <h3 className="EductionFundStyles">
+                Education Fund
+                <br /> estimate
+              </h3>
+              <p className="MoreEductionFundStyles">
+                This calculator helps you determine how much you
+                <br /> will need for school within a number of years
+              </p>
+              <div className="MarginTopForButton">
+                <div className="df">
+                  <div>
+                    <input
+                      type="button"
+                      value="Estimate your education funds"
+                      className="EstimateBTNStyles"
+                    />
+                  </div>
+                  <div className="PositionArrowImage">
+                    <img src={RightArrImage} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="MoneyAfricaToolsCard">
+              <h3>Savings Investment</h3>
+              <p>
+                This calculator helps you determine how much you
+                <br /> need to save monthly to meet a targeted amount
+                <br /> by the time you need it.
+              </p>
+              <div className="MarginTopForButton">
+                <div className="df">
+                  <div>
+                    <input type="button" value="Calculate your savings" />
+                  </div>
+                  <div>
+                    <img src={RightArrImage} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
