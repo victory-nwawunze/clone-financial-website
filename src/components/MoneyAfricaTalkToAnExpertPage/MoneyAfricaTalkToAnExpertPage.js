90d44adb310753df27417bdb59c50a10d693d42f
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-28 08:49:16 -0700

    Creating MoneyAfricaTalkToAnExpertPage

--- src/components/MoneyAfricaTalkToAnExpertPage/MoneyAfricaTalkToAnExpertPage.js
+++ src/components/MoneyAfricaTalkToAnExpertPage/MoneyAfricaTalkToAnExpertPage.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-
+import UserIcon from "../../images/UserIcon.png";
 const MoneyAfricaTalkToAnExpertPage = () => {
   return (
     <React.Fragment>
@@ -13,6 +13,30 @@ const MoneyAfricaTalkToAnExpertPage = () => {
           Fill out the form below and one of our experts will get in touch with
           you.
         </p>
+      </div>
+      <div className="StyleMoneyAfricaTalkToAnExpertForm">
+        <div className="MoneyAfricaTalkToAnExpertFormTextStyles">
+          <div className="df justify-content-betw">
+            <div>
+              <p className="FNstyles">First name</p>
+              <div className="PaddingUserIconAndTextStyles">
+                <input type="text" className="FNUserInput" />
+                <div className="PositionUserIcon">
+                  <img src={UserIcon} />
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className="FNstyles">First name</p>
+              <div className="PaddingUserIconAndTextStyles">
+                <input type="text" className="FNUserInputSecond" />
+                <div className="PositionUserIcon">
+                  <img src={UserIcon} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
