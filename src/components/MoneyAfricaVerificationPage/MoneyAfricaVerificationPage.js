e77dfe5e81531885ae5c780a42010cb6a392a0ef
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-01 08:32:13 -0700

    Creating MoneyAfricaVerificationPage

--- src/components/MoneyAfricaVerificationPage/MoneyAfricaVerificationPage.js
+++ src/components/MoneyAfricaVerificationPage/MoneyAfricaVerificationPage.js
@@ -10,12 +10,34 @@ const MoneyAfricaVerificationPage = () => {
           <img src={MoneyAfricaVerificationLogoImage} />
         </div>
         <h3 className="EmailVerificationHeadingText">Email Verification</h3>
-        <p>
+        <p className="EmailVerificationDesText">
           Protecting your account is our top priority. A verification
           <br /> code has been sent to your email address. Please input the{" "}
           <br />
           code below to complete your registration.
         </p>
+        <div className="MoneyAficaBoxesInput">
+          <div className="df">
+            <div>
+              <input type="text" placeholder="5" className="box-1" />
+            </div>
+            <div>
+              <input type="text" placeholder="5" className="box-2" />
+            </div>
+            <div>
+              <input type="text" placeholder="5" className="box-3" />
+            </div>
+            <div>
+              <input type="text" placeholder="5" className="box-4" />
+            </div>
+            <div>
+              <input type="text" placeholder="5" className="box-5" />
+            </div>
+            <div>
+              <input type="text" placeholder="5" className="box-6" />
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
