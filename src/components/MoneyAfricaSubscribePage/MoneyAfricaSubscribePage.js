c81660e2fad4cd7e785eb386cc673fa4eb81b8e3
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-02 05:56:11 -0700

    MoneyAfricaSubscribePageCards

--- src/components/MoneyAfricaSubscribePage/MoneyAfricaSubscribePage.js
+++ src/components/MoneyAfricaSubscribePage/MoneyAfricaSubscribePage.js
@@ -5,72 +5,11 @@ import MoneyAfricaGreenLogo from "../../images/MoneyAfricaLogoGreen.png";
 import SubScribePageCardPriceOne from "../../images/SubScribePageCardPriceOne.png";
 import SmallCirlceMarkedIcon from "../../images/SmallCirlceMarkedIcon.png";
 import IllustrationChartImage from "../../images/IllustrationChartImage.png";
+import WhiteCirlceMarkedIcon from "../../images/WhiteCirlceMarkedIcon.png";
 const MoneyAfricaSubscribePage = () => {
   return (
     <React.Fragment>
-      <div className="MarginMoneyAfricaGreenLogo">
-        <img src={MoneyAfricaGreenLogo} />
-      </div>
-      <h3 className="ChoosePlanHeadingText">Choose your preferred plan</h3>
-      <p className="ChoosePlanDecText">
-        Choose a plan to procees and have access to all the
-        <br /> video and article courses on our platform.
-      </p>
-      <div className="MarginSubscribeCardPageStyle">
-        <div className="df justify-content-betw">
-          <div>
-            <div className="SubscribePageCardOne">
-              <div className="MarginCardPricing">
-                <div className="df">
-                  <div>
-                    <img src={SubScribePageCardPriceOne} />
-                  </div>
-                  <div>
-                    <h4 className="PaddingTopWalletTextStyles">
-                      ₦5,000
-                      <br />
-                      <span className="StyleMarginCardSubStyles">
-                        {" "}
-                        / 3 months
-                      </span>
-                    </h4>
-                  </div>
-                </div>
-                <div className="PaddingTextWithMarkedIcon">
-                  <div className="df">
-                    <div className="PaddingTopMarkedIcon">
-                      <img src={SmallCirlceMarkedIcon} />
-                    </div>
-                    <div>
-                      <p className="PaddingSubscribeCardText">
-                        Autorenews every 3 months.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="PaddingTextWithMarkedIcon">
-                  <div className="df">
-                    <div className="PaddingTopMarkedIconTwo">
-                      <img src={SmallCirlceMarkedIcon} />
-                    </div>
-                    <div>
-                      <p className="PaddingSubscribeCardText">
-                        Access to everything - video &<br /> article courses.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="PaddingTextCenterForRtext">
-              <p>Recommended</p>
-            </div>
-          </div>
-          <div>3</div>
-        </div>
-      </div>
+      <h1>MoneyAfricaSubscribePage</h1>
     </React.Fragment>
   );
 };
