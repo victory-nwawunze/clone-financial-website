912b292f112d01fffb4f4b7134a3ab89285ae93d
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-02 07:58:28 -0700

    Finished MoneyAfricaLoginPage

--- src/components/MoneyAfricaLoginPage/MoneyAfricaLoginPage.js
+++ src/components/MoneyAfricaLoginPage/MoneyAfricaLoginPage.js
@@ -3,6 +3,7 @@
 import React from "react";
 import MoneyAfricaGreenLogoImage from "../../images/MoneyAfricaGreenLogoImage.png";
 import EmailIcon from "../../images/EmailIcon.png";
+import HidePassword from "../../images/HidePassword.png";
 const MoneyAfricaLoginPage = () => {
   return (
     <React.Fragment>
@@ -27,9 +28,30 @@ const MoneyAfricaLoginPage = () => {
         <div className="MarginForOneInputField">
           <p className="EmailAdressTextStyles">Email Address</p>
           <div className="PositionEmailIcon">
-            <img src={EmailIcon} />
+            <div className="df justify-content-betw align-items-centr">
+              <div>
+                <img src={EmailIcon} />
+              </div>
+              <div>
+                <img src={HidePassword} />
+              </div>
+            </div>
           </div>
           <input type="text" className="MoneyAfricaEmailInputField" />
+          <p className="RPTextStyles">Forgot Password?</p>
+          <div className="MarginSingUpBTNAndLoginBTN">
+            <div className="df justify-content-betw align-items-centr">
+              <div>
+                <p className="SignUpTextStyles">
+                  Have no account yet?{" "}
+                  <span className="InnerSignUpTextStyles">Sign Up</span>
+                </p>
+              </div>
+              <div>
+                <input type="button" className="SignUpBTN" value="Log in" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
