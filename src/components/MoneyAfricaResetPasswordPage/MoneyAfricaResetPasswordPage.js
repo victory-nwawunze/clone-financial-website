b7cc7ad144a00fb57bf8076ddfaf9f69e030d1ee
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-03 04:32:44 -0700

    Creating MoneyAfricaResetPasswordPage

--- src/components/MoneyAfricaResetPasswordPage/MoneyAfricaResetPasswordPage.js
+++ src/components/MoneyAfricaResetPasswordPage/MoneyAfricaResetPasswordPage.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 import MoneyAfricaGreenLogo from "../../images/MoneyAfricaGreenLogoImage.png";
+import LockIcon from "../../images/LockIcon.png";
+import HidePassword from "../../images/HidePassword.png";
 const MoneyAfricaResetPasswordPage = () => {
   return (
     <React.Fragment>
@@ -13,6 +15,36 @@ const MoneyAfricaResetPasswordPage = () => {
         <p className="MoreResetPasswordTextStyles">
           Enter your new password for your account.
         </p>
+        <div className="CenterMarginMoneyAfricaResetPasswordPageStyles">
+          <div className="PaddingForAllInputForResetPage">
+            <p className="MoneyAfricaRPtextStyles">New Password</p>
+            <div className="FlexBothHidePasswordAndLockPassword">
+              <div className="df justify-content-betw">
+                <div>
+                  <img src={LockIcon} />
+                </div>
+                <div>
+                  <img src={HidePassword} />
+                </div>
+              </div>
+            </div>
+            <input type="text" className="StyleResetPasswordInput" />
+          </div>
+          <div className="PaddingForAllInputForResetPage">
+            <p className="MoneyAfricaRPtextStyles">New Password</p>
+            <div className="FlexBothHidePasswordAndLockPassword">
+              <div className="df justify-content-betw">
+                <div>
+                  <img src={LockIcon} />
+                </div>
+                <div>
+                  <img src={HidePassword} />
+                </div>
+              </div>
+            </div>
+            <input type="text" className="StyleResetPasswordInput" />
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
