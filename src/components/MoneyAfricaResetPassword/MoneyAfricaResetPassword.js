a1c184c4ea706cbe8fcedb02fedd9892dcc0e6f8
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-03 02:03:05 -0700

    Creating MoneyAfricaResetPasswordPage

--- src/components/MoneyAfricaResetPassword/MoneyAfricaResetPassword.js
+++ src/components/MoneyAfricaResetPassword/MoneyAfricaResetPassword.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import MoneyAfricaGreenLogo from "../../images/MoneyAfricaGreenLogoImage.png";
+import EmailIcon from "../../images/EmailIcon.png";
 const MoneyAfricaResetPassword = () => {
   return (
     <React.Fragment>
@@ -13,6 +14,13 @@ const MoneyAfricaResetPassword = () => {
         Please provide the email address you used when you
         <br /> signed up for your MoneyAfrica account.
       </p>
+      <div className="MoneyAfricaResetPageInputStyles">
+        <p className="ETextInputTextStyles">Email Address</p>
+        <div className="PositionEmailTextStyles">
+          <img src={EmailIcon} />
+        </div>
+        <input type="text" className="EmailTextInputTextStyle" />
+      </div>
     </React.Fragment>
   );
 };
